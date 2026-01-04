a94419a0025289e38abeb6458d3d85fa0f9e7d71
Mukesh Kharb <108737400+478mkharb@users.noreply.github.com>
2026-01-04 17:36:47 +0530

    Create Jenkins pipeline with demo folders and jobs
    Defines a Jenkins pipeline with two jobs and folder structure.

---
+++ demo_dsl.groovy
@@ -0,0 +1,55 @@
+folder('demo1') {
+    description('Parent folder')
+}
+
+folder('demo1/demo2') {
+    description('Nested folder')
+}
+
+def sharedWs = '/home/jenkins/java-shared-wspace'
+
+job('demo1/job1') {
+    description('Checkout Java project source code')
+
+    customWorkspace(sharedWs)
+    concurrentBuild(false)
+
+    scm {
+        git {
+            remote {
+                url('https://github.com/opstree/spring3hibernate.git')
+            }
+            branch('*/master')
+        }
+    }
+
+    steps {
+        shell '''
+            echo "Running demo1/job1"
+            echo "Workspace: $(pwd)"
+            ls -l
+        '''
+    }
+}
+
+job('demo1/demo2/job2') {
+    description('Build using source from job1 workspace')
+
+    customWorkspace(sharedWs)
+    concurrentBuild(false)
+
+    steps {
+        shell '''
+            echo "Running demo1/demo2/job2"
+            echo "Workspace: $(pwd)"
+
+            # safety check
+            if [ ! -f pom.xml ]; then
+                echo "ERROR: Source code not found. Run job1 first."
+                exit 1
+            fi
+
+            mvn clean package -DskipTests
+        '''
+    }
+}
